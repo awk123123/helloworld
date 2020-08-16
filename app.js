bb91530337d5f630674a9252201ec8c5d9eb1d5d
Jeffrey Chu <chuje2@oocl.com>
2020-08-17 01:10:53 +0800

    second try

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Rita BB I LOVE U 3000 <3')
+  res.send('Rita BB I LOVE U 3000 <3<3<3<3')
 })
 
 app.listen(port, () => {
