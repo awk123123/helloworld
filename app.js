9ed5e826aba7ea0b478271812cbbba0defba5fae
Jeffrey Chu <chuje2@oocl.com>
2020-08-17 02:27:57 +0800

    kubernetes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Rita BB I LOVE U 3000 <3<3<3<3')
+  res.send('Rita BB I LOVE U 3000 <3<3<3<3\nTry Kubernetes continuous deployment!!!!')
 })
 
 app.listen(port, () => {
