89f736533e76a8a8a2c89598d3b13348d80b1753
Jeffrey Chu <chuje2@oocl.com>
2020-08-17 12:35:32 +0800

    auto pull and deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Rita BB I LOVE U 3000 <3<3<3<3\nTry Kubernetes continuous deployment!!!!')
+  res.send('Keel auto pull image and deploy!!!')
 })
 
 app.listen(port, () => {
